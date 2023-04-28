18538888dd3e160a1c9439822656485ba5953df4
Vanishah30 <vanisha.shah@gmail.com>
2023-04-28 11:26:23 +0200

    changed to the baseUrl in server with the domain www.vanisha.nodehill.com to go live

--- baseUrl.js
+++ baseUrl.js
@@ -1,2 +1,4 @@
 // Change to the url of the project you are testing
 module.exports = 'http://localhost:3000'
+
+// changed to the baseUrl in server with the domain www.vanisha.nodehill.com to go live
